783dab8692385badb8f858ac94d52f12f0919d3e
Helen Guo <45088633+ghl1024@users.noreply.github.com>
2019-12-12 22:04:45 +0800

    Python脚本巡检Linux系统

--- Python-Linux.py
+++ Python-Linux.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-'''
-1、安装psutil模块：
-wget https://pypi.python.org/packages/e2/e1/600326635f97fee89bf8426fef14c5c29f4849c79f68fd79f433d8c1bd96/psutil-5.4.3.tar.gz
-tar -xf psutil-5.4.3.tar.gz
-cd psutil-5.4.3
-python3 setup.py install
-
-2、安装xlsxwriter模块：
-pip install XlsxWriter
-'''
-
 import psutil, time, xlsxwriter
 from  xlsxwriter import Workbook
 import socket
